799de071234dfa2b30d848360fceaa427b9ab7a6
juliwithoutthee <jnboucree@gmail.com>
2022-04-27 17:08:57 +0900

    Adds fake users including persona to user seed

--- server/db/seeds/01_user.js
+++ server/db/seeds/01_user.js
@@ -3,11 +3,11 @@
  * @returns { Promise<void> } 
  */
 exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+
+  
+  await knex('user').insert([
+    {id: 1, email: 'miguelito@yomail.com', username: "miguelito", first_name:"Miguel", last_name:"Long", password:"1234"},
+    {id: 2, email: 'lucasfire@yomail.com', username: "firelucas", first_name:"Lucas", last_name:"Flamel", password:"4567"},
+    {id: 3, email: 'maxineko@yomail.com', username: "maxinekoala", first_name:"Maxine", last_name:"Ko", password:"456789"}
   ]);
 };

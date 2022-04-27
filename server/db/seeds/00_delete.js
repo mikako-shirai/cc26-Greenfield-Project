5f6f858d9f8eca84141c14216c7d2847cccc9b92
juliwithoutthee <jnboucree@gmail.com>
2022-04-27 17:10:02 +0900

    Updates seed for delete

--- server/db/seeds/00_delete.js
+++ server/db/seeds/00_delete.js
@@ -4,10 +4,6 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  await knex('user').del();
+  await knex('calendar').del();
 };

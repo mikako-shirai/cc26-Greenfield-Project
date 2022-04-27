7135495dd20bbd6902177558bb864f6f40e56b73
juliwithoutthee <jnboucree@gmail.com>
2022-04-27 15:41:17 +0900

    Updates calendar_table

--- server/db/migrations/20220427055622_create_calendar_table.js
+++ server/db/migrations/20220427055622_create_calendar_table.js
@@ -2,14 +2,20 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema
+        .createTable("calendar", function (table) {
+            table.increments("id").unique().notNullable();
+            table.integer("userId"); // gets from user table;
+            table.date("date");
+            table.string("events");
+        })
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    knex.schema.dropTable("calendar");
 };

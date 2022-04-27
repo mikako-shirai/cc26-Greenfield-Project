bd220627226e92638c0c5c337f6b9047f9ddbf68
mikako-shirai <mkkshirai@gmail.com>
2022-04-27 14:45:27 +0900

    chore: update package.json

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const db = require("./db");
-
-const PORT = process.env.PORT || 80;
 const app = express();
+const db = require("./db");
+const PORT = process.env.PORT || 80;
 
 app.use(express.json());
 app.use("/", express.static(__dirname + "/public"));

98efb1516329fc7a6394690d2e1d395eb1ef3c98
mikako-shirai <mkkshirai@gmail.com>
2022-04-27 16:30:27 +0900

    refactor: update index.js

--- server/index.js
+++ server/index.js
@@ -8,6 +8,32 @@ app.use(express.json());
 app.use("/", express.static(__dirname + "../public"));
 
 app.get("/home", async (req, res) => {
+  console.log("from inde.js");
+  res.status(500);
+});
+
+app.get("/login", async (req, res) => {
+  console.log("from inde.js");
+  res.status(500);
+});
+
+app.get("/signup", async (req, res) => {
+  console.log("from inde.js");
+  res.status(500);
+});
+
+app.get("/docs", async (req, res) => {
+  console.log("from inde.js");
+  res.status(500);
+});
+
+app.get("/tasks", async (req, res) => {
+  console.log("from inde.js");
+  res.status(500);
+});
+
+app.get("/events", async (req, res) => {
+  console.log("from inde.js");
   res.status(500);
 });
 

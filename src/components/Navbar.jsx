44b7e0a98f3271a38b83ea98d80e5607dcb7ddd4
stk-m <stk.murakami@gmail.com>
2022-04-28 19:42:46 +0900

    feat: add implementation to Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import Search from "./Searchbtn.jsx";
 import Calendar from "react-calendar";
 import Document from "./Document.jsx";
@@ -7,17 +7,36 @@ import Event from "./Event.jsx";
 
 
 function Navbar() {
+
+  const [showCalendar, setCalendar] = useState(true);
+  const [showDocument, setDocument] = useState(true);
+  const [showTask, setTask] = useState(true);
+  const [showEvent, setEvent] = useState(true);
+  
   return (
     <div className="navbar">
       <Search />
-      <Calendar />
-      {/* <Document />
-      <Task />
-      <Event /> */}
-      {/* <a href="calendar">Calendar</a>
-      <a href="docs">Docs</a>
-      <a href="task">Task</a>
-      <a href="event">Events</a> */}
+
+      {showCalendar && <a onClick={()=>{
+        setCalendar(false)
+      }}>Calender</a> 
+      || <Calendar/>}
+
+      {showDocument && <a onClick={()=>{
+        setDocument(false)
+      }}>Document</a> 
+      || <Document/>}
+
+      {showTask && <a onClick={()=>{
+        setTask(false)
+      }}>Task</a> 
+      || <Task/>}
+
+      {showEvent && <a onClick={()=>{
+        setEvent(false)
+      }}>Event</a> 
+      || <Event/>}
+
     </div>
   );
 }

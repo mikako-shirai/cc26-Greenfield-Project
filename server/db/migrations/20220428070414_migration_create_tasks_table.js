62a524125849c2f98542924f245523367543c11d
juliwithoutthee <jnboucree@gmail.com>
2022-04-28 20:04:34 +0900

    Updates tasks table

--- server/db/migrations/20220428070414_migration_create_tasks_table.js
+++ server/db/migrations/20220428070414_migration_create_tasks_table.js
@@ -3,6 +3,13 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
+    return knex.schema
+    .createTable("tasks", function (table) {
+        table.increments("id").primary();
+        table.string("task_name");
+        table.string("task_info");
+        table.datetime("date_time");
+    })
   
 };
 
@@ -11,5 +18,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+    knex.schema.dropTable("tasks");
 };

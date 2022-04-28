7ef85de57b97ba89fdfd41c847f9909783364f65
lzs95 <FuckYou@hotmail.com>
2022-04-28 11:05:41 +0900

    styles: calendarComponent CSS implementation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,18 @@
 import { React, useState } from "react";
 import "../styles/index.css";
+// import "react-calendar/dist/Calendar.css";
 import Navbar from "./Navbar.jsx";
 import Task from "./Task.jsx";
+import CalendarComponent from "./Calendar.jsx";
 
 function App() {
   const [show, toggleShow] = useState(true);
   return (
-    <div className="App">
+    <div className="app">
       <Navbar />
+      <div>
+        <CalendarComponent />
+      </div>
     </div>
   );
 }

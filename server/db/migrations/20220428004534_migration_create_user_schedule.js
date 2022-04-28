a81af6ed21cfacdd9afd84d9862c37ecb03b11c0
juliwithoutthee <jnboucree@gmail.com>
2022-04-28 09:52:15 +0900

    Updates user_schedule table in migrations

--- server/db/migrations/20220428004534_migration_create_user_schedule.js
+++ server/db/migrations/20220428004534_migration_create_user_schedule.js
@@ -3,6 +3,16 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
+    knex.schema
+    .createTable("user_schedule", function (table) {
+        table.increments("id").unique().notNullable();
+        table.integer("userId"); // gets from user table;
+        table.date("date");
+        table.string("events");
+        table.string("tasks");
+        table.string("docs");
+        table.string("info");
+    })
   
 };
 
@@ -11,5 +21,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+    knex.schema.dropTable("user_schedule");
 };

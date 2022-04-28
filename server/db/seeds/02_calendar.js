1579426f9ac71e1b1cb786351c94d469357f9b68
juliwithoutthee <jnboucree@gmail.com>
2022-04-28 11:23:00 +0900

    Updates calendar seed based on new columns

--- server/db/seeds/02_calendar.js
+++ server/db/seeds/02_calendar.js
@@ -6,8 +6,8 @@ exports.seed = async function(knex) {
 
   
   await knex('calendar').insert([
-    {id: 1, userId: 'rowValue1', date: "", events:""},
-    {id: 2, userId: 'rowValue2', date: "", events:""},
-    {id: 3, userId: 'rowValue3', date: "", events:""}
+    {id: 1, userId: '1', date: "2022-04-29", day:"29", month:"04", year:"2022", dayOfWeek:"Monday", time:"12:00"},
+    {id: 2, userId: '1', date: "2022-04-30", day:"30", month:"04", year:"2022", dayOfWeek:"Monday", time:"09:00"},
+    {id: 3, userId: '1', date: "2022-05-01", day:"01", month:"05", year:"2022", dayOfWeek:"Monday", time:"11:00"}
   ]);
 };

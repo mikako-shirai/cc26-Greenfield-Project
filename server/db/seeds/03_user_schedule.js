ee595b75d648f57b260a09badb4c9a03868e3c77
juliwithoutthee <jnboucree@gmail.com>
2022-04-28 10:57:29 +0900

    Updates user_schedule seed

--- server/db/seeds/03_user_schedule.js
+++ server/db/seeds/03_user_schedule.js
@@ -2,12 +2,11 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
-exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+exports.seed = async function (knex) {
+
+  await knex('user_schedule').insert([
+    { id: 1, userId: '1', date: "", time: "", events: "study session", tasks: "study Algorithms", docs: "https://en.wikipedia.org/wiki/Algorithm", comments: "yoooooooo" },
+    { id: 2, userId: '1', date: "", time: "", events: "study session", tasks: "study Japanese", docs: "https://jisho.org", comments: "yoooooooo" },
+    { id: 3, userId: '1', date: "", time: "", events: "study session", tasks: "study Javascript", docs: "https://developer.mozilla.org/en-US/", comments: "yoooooooo" }
   ]);
 };

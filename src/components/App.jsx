46caacbf63d0f41e3365069e0d4a434b6fe6c24c
stk-m <stk.murakami@gmail.com>
2022-04-28 20:08:34 +0900

    feat: implement the click event to App.jsx and Navbar.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,25 +2,57 @@ import { React, useState } from "react";
 import "../styles/index.css";
 import Navbar from "./Navbar.jsx";
 import CalendarComponent from "./Calendar.jsx";
+import Document from "./Document";
 import Event from "./Event.jsx";
+import Calendar from "react-calendar/dist/umd/Calendar";
+import Task from "./Task";
 
 function App() {
   const [selectedDate, setSelectedDate] = useState(null);
   const [showTask, setShowTask] = useState(false);
 
+  const [showCalendar, setCalendar] = useState(false);
+  const [showDocument, setDocument] = useState(false);
+  const [showTaskList, setTaskList] = useState(false);
+  const [showEvent, setEvent] = useState(false);
+
+  const displayCalender = () => {
+    setCalendar(true);
+  };
+  const displayDocument = () => {
+    setDocument(true);
+  };
+  const displayTaskList = () => {
+    setTaskList(true);
+  };
+  const displayEvent = () => {
+    setEvent(true);
+  };
+
   return (
     <div className="app">
-      <Navbar />
-      <Event />
 
+      <Navbar 
+      displayCalender={displayCalender}
+      displayDocument={displayDocument}
+      displayTaskList={displayTaskList}
+      displayEvent={displayEvent} 
+      />
+      
       <div>
         <CalendarComponent
           selectedDate={selectedDate}
           setSelectedDate={setSelectedDate}
-          showTask={showTask}
+          showTask={showTaskList}
           setShowTask={setShowTask}
         />
       </div>
+
+      {showCalendar && <Calendar />}
+      {showDocument && <Document />}
+      {showTaskList && <Task />}
+      {showEvent && <Event />}
+
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,15 @@
-import React,{useState} from "react";
+import React from "react";
 import Search from "./Searchbtn.jsx";
-import Calendar from "react-calendar";
-import Document from "./Document.jsx";
-import Task from "./Task.jsx";
-import Event from "./Event.jsx";
 
+function Navbar({ displayCalender, displayDocument, displayTaskList, displayEvent }) {
 
-function Navbar() {
-
-  const [showCalendar, setCalendar] = useState(true);
-  const [showDocument, setDocument] = useState(true);
-  const [showTask, setTask] = useState(true);
-  const [showEvent, setEvent] = useState(true);
-  
   return (
     <div className="navbar">
       <Search />
-
-      {showCalendar && <a onClick={()=>{
-        setCalendar(false)
-      }}>Calender</a> 
-      || <Calendar/>}
-
-      {showDocument && <a onClick={()=>{
-        setDocument(false)
-      }}>Document</a> 
-      || <Document/>}
-
-      {showTask && <a onClick={()=>{
-        setTask(false)
-      }}>Task</a> 
-      || <Task/>}
-
-      {showEvent && <a onClick={()=>{
-        setEvent(false)
-      }}>Event</a> 
-      || <Event/>}
+      <a onClick={displayCalender}>Calendar</a> 
+      <a onClick={displayDocument}>Document</a>
+      <a onClick={displayTaskList}>Task</a>
+      <a onClick={displayEvent}>Event</a> 
 
     </div>
   );

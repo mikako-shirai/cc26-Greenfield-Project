c2d7be649556e860ef6dffa1beb11c24f9d490ef
mikako-shirai <mkkshirai@gmail.com>
2022-04-28 19:14:27 +0900

    style: clean up previous code

--- server/server.js
+++ server/server.js
@@ -4,9 +4,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: 'http://localhost:8080'
-}));
+app.use(cors({ origin: 'http://localhost:8080' }));
 const PORT = process.env.PORT || 8080;
 
 // app.use(express.static(__dirname + "../public"));

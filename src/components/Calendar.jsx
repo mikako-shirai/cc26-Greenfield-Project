e5a1f66b838e551391e9ada71a1aaed24991291b
lzs95 <lfzs95@gmail.com>
2022-04-29 16:15:13 +0900

    feat: create array of data from Task.jsx modal

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,5 +1,5 @@
 import Calendar from "react-calendar";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Task from "./Task.jsx";
 
 function CalendarComponent({
@@ -8,15 +8,26 @@ function CalendarComponent({
   showTask,
   setShowTask,
 }) {
+  const [newToDo, setNewToDo] = useState(false);
   //Displays modal and Pass Selected Date
   const openWindowForDate = (date) => {
     setSelectedDate(date.toISOString());
   };
 
+  useEffect(() => {
+    console.log("WORKING", newToDo);
+    setNewToDo(false);
+  }, [newToDo]);
   //When you call (onCLickDay) in the calendar it will display Task modal, Isosstringify current date and send it to Task.jsx component
   return (
     <div className="calendar">
-      {showTask && <Task date={selectedDate} setShowTask={setShowTask} />}
+      {showTask && (
+        <Task
+          date={selectedDate}
+          setShowTask={setShowTask}
+          setNewToDo={setNewToDo}
+        />
+      )}
       <Calendar
         className="react-calendar"
         onClickDay={(value) => {

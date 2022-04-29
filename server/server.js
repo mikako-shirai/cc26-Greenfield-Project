caebab3d6dfe343b558479b3d91876e9e3c66090
mikako-shirai <mkkshirai@gmail.com>
2022-04-30 00:20:59 +0900

    chore: update files

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: 'http://localhost:8080/' }));
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "public")));
 const PORT = process.env.PORT || 8080;
 
 const usersController = require("./users/users.controller");

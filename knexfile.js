ca072819bdbb765b10560dd9a2a6ef51aa1e2410
Thomas Ryan <DWTrip@gmail.com>
2022-05-02 18:48:32 +0900

    Frontend: bug: production knexfile did not have seed directory listed. Now it does.

--- knexfile.js
+++ knexfile.js
@@ -41,5 +41,6 @@ module.exports = {
     migrations: {
       directory: "./server/db/migrations",
     },
+    seeds: { directory: "./server/db/seeds" },
   },
 };
--- server/db/migrations/20220427050542_create_user_table.js
+++ server/db/migrations/20220427050542_create_user_table.js
@@ -3,22 +3,14 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema
-    .createTable("user", function (table) {
-      table.increments("id")
-        .primary();
-      table.string("email", 32)
-        .unique()
-        .notNullable()
-        .index(); 
-      table.string("username", 32)
-        .unique()
-        .notNullable();
-      table.string("first_name", 32).notNullable();
-      table.string("last_name", 32).notNullable();
-      table.string("password", 32)
-        .notNullable();
-    })
+  return knex.schema.createTable("user", function (table) {
+    table.increments("id").primary();
+    table.string("email", 32).unique().notNullable().index();
+    table.string("username", 32).unique().notNullable();
+    table.string("first_name", 32).notNullable();
+    table.string("last_name", 32).notNullable();
+    table.string("password", 32).notNullable();
+  });
 };
 
 /**

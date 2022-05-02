b03622a9e3631c323f38d4e8b2883b7d1e7cdd69
Thomas Ryan <DWTrip@gmail.com>
2022-05-02 15:54:43 +0900

    Heroku: bug: clean up old code

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-
+import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./components/App";
+import "bootstrap/dist/css/bootstrap.css";
 
-import { createRoot } from "react-dom/client";
-const container = document.getElementById("root");
-const root = createRoot(container);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />

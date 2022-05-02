20c3ba9e69db384d85aa95a45501eff9b98c9bd6
Thomas Ryan <DWTrip@gmail.com>
2022-05-02 18:26:37 +0900

    Frontend: chore/bug: Reorganized frontend so it has a seperate package.json file from backend.

--- server/docs/docs.controller.js
+++ server/docs/docs.controller.js
@@ -2,12 +2,14 @@ const docsModel = require("./docs.model");
 
 module.exports = {
   async index(req, res) {
+    console.log("Get request for docs");
     const docs = await docsModel.getAll();
     res.send(docs);
   },
 
   async view(req, res) {
     const id = parseInt(req.params.id);
+    // console.log("This is the thing I am looking at", id);
     const doc = await docsModel.getById(id);
     res.send(doc);
   },
@@ -17,12 +19,11 @@ module.exports = {
 
     const docObj = {
       doc,
-      date_time: dateTime
+      date_time: dateTime,
     };
 
-    id ? await docsModel.update(id, docObj)
-       : await docsModel.create(docObj);
-    
+    id ? await docsModel.update(id, docObj) : await docsModel.create(docObj);
+
     res.status(200).send();
-  }
+  },
 };

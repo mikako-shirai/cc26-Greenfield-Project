153d63d52482aa6c2e902c513b08524eceabc25b
Thomas Ryan <DWTrip@gmail.com>
2022-05-02 15:45:55 +0900

    Heroku: bug: deleted proxy from package.json

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // app.use(cors(corsOptions));
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 const usersController = require("./users/users.controller");
 const schedulesController = require("./schedules/schedules.controller");
